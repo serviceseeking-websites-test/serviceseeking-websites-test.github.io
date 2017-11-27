0874787ab9548534854c0d31eb3964938c6d323e
Gem <gemayma.sanluis@gmail.com>
2017-11-27 08:48:59 +0800

    websites

---
+++ C & C Group/C & C Group _ C & C Group_files/app.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+	var $portfolioClone = $(".portfolio").clone();
+	$(".quicksand a").click(function(e) {
+		e.preventDefault();
+		$(".quicksand li").removeClass("current");
+		var $filterClass = $(this).parent().attr("class");
+
+		if ($filterClass == "all") {
+			var $filteredPortfolio = $portfolioClone.find("li");
+		} else {
+			var $filteredPortfolio = $portfolioClone.find("li[data-type~="+$filterClass+"]");
+		}
+
+		$(".portfolio").quicksand( $filteredPortfolio, {
+			duration: 800,
+			easing: 'easeInOutQuad'
+		});
+		$(this).parent().addClass("current");
+	});
+  
+   $(".quicksand-month a").click(function(e) {
+		e.preventDefault();
+		$(".quicksand-month li").removeClass("current");
+		var $filterClass = $(this).parent().attr("class");
+
+		if ($filterClass == "all") {
+			var $filteredPortfolio = $portfolioClone.find("li");
+		} else {
+			var $filteredPortfolio = $portfolioClone.find("li[data-date~="+$filterClass+"]");
+		}
+
+		$(".portfolio").quicksand( $filteredPortfolio, {
+			duration: 800,
+			easing: 'easeInOutQuad'
+		});
+		$(this).parent().addClass("current");
+	});
+});
